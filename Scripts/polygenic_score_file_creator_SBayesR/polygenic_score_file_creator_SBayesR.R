f8635da5624014a77b0035300cfee0726b6d730b
Remo Monti <Remo.Monti@vm-cpu01.dhclab.i.hpi.de>
2021-07-06 13:10:33 +0000

    SBayesR now runs both with --robust and --rsq 0.95 (two separate outputs)

--- Scripts/polygenic_score_file_creator_SBayesR/polygenic_score_file_creator_SBayesR.R
+++ Scripts/polygenic_score_file_creator_SBayesR/polygenic_score_file_creator_SBayesR.R
@@ -1,5 +1,6 @@
 #!/usr/bin/Rscript
 # This script was written by Oliver Pain whilst at King's College London University.
+# The script was adjusted by Remo Monti to allow for other parameter settings.
 start.time <- Sys.time()
 suppressMessages(library("optparse"))
 
@@ -32,6 +33,8 @@ make_option("--P_max", action="store", default=NA, type='numeric',
     help="P-value threshold for filter variants [optional]"),
 make_option("--robust", action="store", default=F, type='logical',
     help="Force robust GCTB parameterisation [optional]"),
+make_option("--force_ref_frq", action="store", default=NA, type='character',
+    help="Path to per chromosome freq-files containing allele frequencies to be forced"),
 make_option("--test", action="store", default=NA, type='character',
     help="Specify number of SNPs to include [optional]"),
 make_option("--ld_matrix_chr", action="store", default=NA, type='character',
@@ -125,7 +128,7 @@ sink()
 ###
 
 # If OR present, calculate BETA
-if(sum(names(GWAS) == 'OR') == 1){
+if((sum(names(GWAS) == 'OR') == 1) & (sum(names(GWAS) == 'BETA') == 0)){
   GWAS$BETA<-log(GWAS$OR)
 }
 
@@ -135,6 +138,31 @@ if(sum(names(GWAS) == 'FREQ') == 1){
 } else {
   GWAS$MAF<-GWAS$REF.FREQ
 }
+
+if (!is.na(opt$force_ref_frq)){
+  # override provided MAF with reference MAF
+  sink(file = paste(opt$output,'.log',sep=''), append = T)
+  cat('--force_ref_freq set, forcing reference allele frequencies\n')
+  sink()
+  
+  freq_files <- list.files(dirname(opt$force_ref_frq), pattern=paste0(basename(opt$force_ref_frq),'.*.frq.*'), include.dirs = T, full.names = T)
+  
+  afreq <- do.call(rbind, lapply(freq_files, fread))
+  
+  # rename
+  setnames(afreq, 'MAF', 'MAF_')
+  setnames(afreq, 'A1', 'A1_')
+  setnames(afreq, 'A2', 'A2_')
+  
+  GWAS <- data.table::merge.data.table(GWAS, afreq) # merge
+      
+  # force reference allele frequencies, keep original allele coding
+  GWAS[, MAF := MAF_]
+  GWAS[(A1 == A2_) & (A2 == A1_), MAF:=(1-MAF)]
+  
+}
+
+
 
 GWAS<-GWAS[,c('SNP','A1','A2','MAF','BETA','SE','P','N'),with=F]
 names(GWAS)<-c('SNP','A1','A2','freq','b','se','p','N')
@@ -205,116 +233,156 @@ CHROMS_vector<-as.numeric(CHROMS_vector)
 CHROMS_vector<-CHROMS_vector[!is.na(CHROMS_vector)] 
 print(CHROMS_vector)
 
+
+# Note: it would be more efficient to parallelize this block together with the steps below...
+if (opt$robust){
+    # perform robust analysis if requested
+    error<-foreach(i=CHROMS_vector, .combine=rbind) %dopar% {
+        if(per_var_N == F & opt$impute_N == T){
+            log<-system(paste0(opt$gctb,' --sbayes R --ldm ',opt$ld_matrix_chr,i,'.ldm.sparse --pi 0.95,0.02,0.02,0.01 --gamma 0.0,0.01,0.1,1 --gwas-summary ',opt$output_dir,'GWAS_sumstats_COJO.txt --chain-length 10000 --robust --exclude-mhc --burn-in 2000 --impute-n --out-freq 1000 --out ',opt$output_dir,'GWAS_sumstats_SBayesR.robust.chr',i), intern=T)
+        } else {
+          log<-system(paste0(opt$gctb,' --sbayes R --ldm ',opt$ld_matrix_chr,i,'.ldm.sparse --pi 0.95,0.02,0.02,0.01 --gamma 0.0,0.01,0.1,1 --gwas-summary ',opt$output_dir,'GWAS_sumstats_COJO.txt --chain-length 10000 --robust --exclude-mhc --burn-in 2000 --out-freq 1000 --out ',opt$output_dir,'GWAS_sumstats_SBayesR.robust.chr',i), intern=T)
+        }
+        writeLines(log, paste0(opt$output_dir,'SBayesR.robust.chr',i,'.log'))
+        if(sum(grepl("MCMC cycles completed", log) == T) == 1 & sum(grepl("Analysis finished", log) == T) == 1){
+            error_log<-data.frame(chr=i, Log='Analysis converged')
+        }
+        if(sum(grepl("MCMC cycles completed", log) == T) == 0 & sum(grepl("Analysis finished", log) == T) == 1){
+            error_log<-data.frame(chr=i, Log='Analysis did not converge')
+        }
+        if(sum(grepl("MCMC cycles completed", log) == T) == 0 & sum(grepl("Analysis finished", log) == T) == 0){
+            error_log<-data.frame(chr=i, Log='Error')
+        }
+        error_log
+    } # dopar exit
+    
+    if(sum(grepl('Error', error$Log) == T) > 1){
+        sink(file = paste(opt$output,'.log',sep=''), append = T)
+        cat('An error occurred for',sum(grepl('Error', error$Log) == T),'chromosomes. Retry requesting more memory or run interactively to debug.\n')
+        print(error)
+        sink()
+        system(paste0('rm ',opt$output_dir,'GWAS_sumstats_*'))
+        stop('Error: SBayesR did not finish regularly.')
+    }
+
+}
+
+# perform standard analysis with --rsq 0.95
 error<-foreach(i=CHROMS_vector, .combine=rbind) %dopar% {
-  if(opt$robust){
     if(per_var_N == F & opt$impute_N == T){
-      log<-system(paste0(opt$gctb,' --sbayes R --ldm ',opt$ld_matrix_chr,i,'.ldm.sparse --pi 0.95,0.02,0.02,0.01 --gamma 0.0,0.01,0.1,1 --gwas-summary ',opt$output_dir,'GWAS_sumstats_COJO.txt --chain-length 10000 --robust --exclude-mhc --burn-in 2000 --impute-n --out-freq 1000 --out ',opt$output_dir,'GWAS_sumstats_SBayesR.chr',i), intern=T)
+        log<-system(paste0(opt$gctb,' --sbayes R --ldm ',opt$ld_matrix_chr,i,'.ldm.sparse --pi 0.95,0.02,0.02,0.01 --gamma 0.0,0.01,0.1,1 --gwas-summary ',opt$output_dir,'GWAS_sumstats_COJO.txt --chain-length 10000 --rsq 0.95 --exclude-mhc --burn-in 2000 --impute-n --out-freq 1000 --out ',opt$output_dir,'GWAS_sumstats_SBayesR.chr',i), intern=T)
     } else {
-      log<-system(paste0(opt$gctb,' --sbayes R --ldm ',opt$ld_matrix_chr,i,'.ldm.sparse --pi 0.95,0.02,0.02,0.01 --gamma 0.0,0.01,0.1,1 --gwas-summary ',opt$output_dir,'GWAS_sumstats_COJO.txt --chain-length 10000 --robust --exclude-mhc --burn-in 2000 --out-freq 1000 --out ',opt$output_dir,'GWAS_sumstats_SBayesR.chr',i), intern=T)
+        log<-system(paste0(opt$gctb,' --sbayes R --ldm ',opt$ld_matrix_chr,i,'.ldm.sparse --pi 0.95,0.02,0.02,0.01 --gamma 0.0,0.01,0.1,1 --gwas-summary ',opt$output_dir,'GWAS_sumstats_COJO.txt --chain-length 10000 --rsq 0.95 --exclude-mhc --burn-in 2000 --out-freq 1000 --out ',opt$output_dir,'GWAS_sumstats_SBayesR.chr',i), intern=T)
     }
-  } else {
-    if(per_var_N == F & opt$impute_N == T){
-      log<-system(paste0(opt$gctb,' --sbayes R --ldm ',opt$ld_matrix_chr,i,'.ldm.sparse --pi 0.95,0.02,0.02,0.01 --gamma 0.0,0.01,0.1,1 --gwas-summary ',opt$output_dir,'GWAS_sumstats_COJO.txt --chain-length 10000 --exclude-mhc --burn-in 2000 --impute-n --out-freq 1000 --out ',opt$output_dir,'GWAS_sumstats_SBayesR.chr',i), intern=T)
-    } else {
-      log<-system(paste0(opt$gctb,' --sbayes R --ldm ',opt$ld_matrix_chr,i,'.ldm.sparse --pi 0.95,0.02,0.02,0.01 --gamma 0.0,0.01,0.1,1 --gwas-summary ',opt$output_dir,'GWAS_sumstats_COJO.txt --chain-length 10000 --exclude-mhc --burn-in 2000 --out-freq 1000 --out ',opt$output_dir,'GWAS_sumstats_SBayesR.chr',i), intern=T)
+    writeLines(log, paste0(opt$output_dir,'SBayesR.chr',i,'.log'))
+    if(sum(grepl("MCMC cycles completed", log) == T) == 1 & sum(grepl("Analysis finished", log) == T) == 1){
+        error_log<-data.frame(chr=i, Log='Analysis converged')
     }
-  }
-  writeLines(log, paste0(opt$output_dir,'SBayesR.chr',i,'.log'))
-  
-	if(sum(grepl("MCMC cycles completed", log) == T) == 1 & sum(grepl("Analysis finished", log) == T) == 1){
-	  error_log<-data.frame(chr=i, Log='Analysis converged')
-	}
-	
-	if(sum(grepl("MCMC cycles completed", log) == T) == 0 & sum(grepl("Analysis finished", log) == T) == 1){
-	  error_log<-data.frame(chr=i, Log='Analysis did not converge')
-	}
-	
-	if(sum(grepl("MCMC cycles completed", log) == T) == 0 & sum(grepl("Analysis finished", log) == T) == 0){
-	  error_log<-data.frame(chr=i, Log='Error')
-	}
-	
-	error_log
+    if(sum(grepl("MCMC cycles completed", log) == T) == 0 & sum(grepl("Analysis finished", log) == T) == 1){
+        error_log<-data.frame(chr=i, Log='Analysis did not converge')
+    }
+    if(sum(grepl("MCMC cycles completed", log) == T) == 0 & sum(grepl("Analysis finished", log) == T) == 0){
+        error_log<-data.frame(chr=i, Log='Error')
+    }
+    error_log
+} # dopar exit
+
+if(sum(grepl('Error', error$Log) == T) > 1){
+    sink(file = paste(opt$output,'.log',sep=''), append = T)
+    cat('An error occurred for',sum(grepl('Error', error$Log) == T),'chromosomes. Retry requesting more memory or run interactively to debug.\n')
+    print(error)
+    sink()
+    # continue even if non-robust analysis failed.
+    # system(paste0('rm ',opt$output_dir,'GWAS_sumstats_*'))
+    # stop('Error: SBayesR did not finish regularly.')
 }
 
 sink(file = paste(opt$output,'.log',sep=''), append = T)
 cat('Done!\n')
 sink()
 
-if(sum(grepl('Error', error$Log) == T) > 1){
-  sink(file = paste(opt$output,'.log',sep=''), append = T)
-    cat('An error occurred for',sum(grepl('Error', error$Log) == T),'chromosomes. Retry requesting more memory or run interactively to debug.\n')
+
+settings <- c('')
+if (opt$robust){
+    settings <- c(settings, 'robust') 
+}
+
+for (setting in settings){
+    if (setting == ''){
+        prefix <- 'GWAS_sumstats_SBayesR.chr'
+    } else {
+        prefix <- paste0('GWAS_sumstats_SBayesR.', setting, '.chr')
+    }
+    # Check whether analysis completed for all chromosomes
+    comp_list<-list.files(path=opt$output_dir, pattern=paste0(prefix,'.*snpRes'))
+    incomp<-c(CHROMS)[!(paste0(prefix,CHROMS,'.snpRes') %in% comp_list)]
+    comp<-c(CHROMS)[(paste0(prefix,CHROMS,'.snpRes') %in% comp_list)]
+                          
+    # Combine per chromosome snpRes files
+    snpRes<-NULL
+    for(i in comp){
+        snpRes<-rbind(snpRes, fread(paste0(opt$output_dir,prefix,i,'.snpRes')))
+    }
+    
+    snpRes<-snpRes[,c('Chrom','Name','Position','A1','A2','A1Effect')]
+    
+    write.table(snpRes, paste0(opt$output_dir,gsub('\\.chr$','.GW.snpRes',prefix)), col.names=F, row.names=F, quote=F)
+    
+    # Combine per chromosome parRes files
+    parRes_mcmc<-list()
+    for(i in comp){
+        parRes_mcmc[[i]]<-fread(paste0(opt$output_dir,prefix,i,'.mcmcsamples.Par'))
+    }
+    
+    parRes<-NULL
+    for(par in names(parRes_mcmc[[i]])){
+        parRes_mcmc_par<-NULL
+        for(i in comp){
+            parRes_mcmc_par<-cbind(parRes_mcmc_par,parRes_mcmc[[i]][[par]])
+        }
+        
+        parRes_mcmc_par_sum<-rowSums(parRes_mcmc_par)
+        
+        parRes_par<-data.frame(	Par=par,
+                                Mean=mean(parRes_mcmc_par_sum),
+                                SD=sd(parRes_mcmc_par_sum))
+                                
+        parRes<-rbind(parRes,parRes_par)
+    }
+                        
+    sink(file = paste(opt$output,'.log',sep=''), append = T)
+    cat('SNP-heritability estimate for setting "',setting,'" is ',parRes[parRes$Par == 'hsq', names(parRes) == 'Mean']," (SD=",parRes[parRes$Par == 'hsq', names(parRes) == 'SD'],").\n", sep='')
+    sink()
+
+    write.table(parRes, paste0(opt$output_dir,gsub('\\.chr$','.GW.parRes',prefix)), col.names=T, row.names=F, quote=F)
+    
+    system(paste0('rm ',opt$output_dir,prefix,'*'))     
+    
+    sink(file = paste(opt$output,'.log',sep=''), append = T)
+    if(length(incomp) >=1){
+        cat('No results for chromosomes',paste(incomp, collapse=','),'\n')
+    }
+    cat('Logs:\n')
     print(error)
-  sink()
-  system(paste0('rm ',opt$output_dir,'GWAS_sumstats_*'))
-  q()
-  n
+    cat('\n')
+    sink()
+    
 }
+    
 
-# Check whether analysis completed for all chromosomes
-comp_list<-list.files(path=opt$output_dir, pattern='GWAS_sumstats_SBayesR.chr.*snpRes')
-incomp<-c(CHROMS)[!(paste0('GWAS_sumstats_SBayesR.chr',CHROMS,'.snpRes') %in% comp_list)]
-comp<-c(CHROMS)[(paste0('GWAS_sumstats_SBayesR.chr',CHROMS,'.snpRes') %in% comp_list)]
-
-# Combine per chromosome snpRes files
-snpRes<-NULL
-for(i in comp){
-  snpRes<-rbind(snpRes, fread(paste0(opt$output_dir,'GWAS_sumstats_SBayesR.chr',i,'.snpRes')))
-}
-
-snpRes<-snpRes[,c('Chrom','Name','Position','A1','A2','A1Effect')]
-
-write.table(snpRes, paste0(opt$output_dir,'GWAS_sumstats_SBayesR.GW.snpRes'), col.names=F, row.names=F, quote=F)
-
-# Combine per chromosome parRes files
-parRes_mcmc<-list()
-for(i in comp){
-	parRes_mcmc[[i]]<-fread(paste0(opt$output_dir,'GWAS_sumstats_SBayesR.chr',i,'.mcmcsamples.Par'))
-}
-
-parRes<-NULL
-for(par in names(parRes_mcmc[[i]])){
-	parRes_mcmc_par<-NULL
-	for(i in comp){
-		parRes_mcmc_par<-cbind(parRes_mcmc_par,parRes_mcmc[[i]][[par]])
-	}
-	
-	parRes_mcmc_par_sum<-rowSums(parRes_mcmc_par)
-	
-	parRes_par<-data.frame(	Par=par,
-							Mean=mean(parRes_mcmc_par_sum),
-							SD=sd(parRes_mcmc_par_sum))
-							
-	parRes<-rbind(parRes,parRes_par)
-}
-
-sink(file = paste(opt$output,'.log',sep=''), append = T)
-cat('SNP-heritability estimate is ',parRes[parRes$Par == 'hsq', names(parRes) == 'Mean']," (SD=",parRes[parRes$Par == 'hsq', names(parRes) == 'SD'],").\n", sep='')
-sink()
-
-write.table(parRes, paste0(opt$output_dir,'GWAS_sumstats_SBayesR.GW.parRes'), col.names=T, row.names=F, quote=F)
-
-system(paste0('rm ',opt$output_dir,'GWAS_sumstats_SBayesR.chr*')) 
-system(paste0('rm ',opt$output_dir,'GWAS_sumstats_COJO.txt')) 
-
-sink(file = paste(opt$output,'.log',sep=''), append = T)
-if(length(incomp) >=1){
-	cat('No results for chromosomes',paste(incomp, collapse=','),'\n')
-}
-cat('Logs:\n')
-print(error)
-cat('\n')
-sink()
-
+                      
 if(!is.na(opt$test)){
-  end.time <- Sys.time()
-  time.taken <- end.time - test_start.time
-  sink(file = paste(opt$output,'.log',sep=''), append = T)
-  cat('Test run finished at',as.character(end.time),'\n')
-  cat('Test duration was',as.character(round(time.taken,2)),attr(time.taken, 'units'),'\n')
-  system(paste0('rm ',opt$output_dir,'*Res')) 
-  sink()
-  q()
+    end.time <- Sys.time()
+    time.taken <- end.time - test_start.time
+    sink(file = paste(opt$output,'.log',sep=''), append = T)
+    cat('Test run finished at',as.character(end.time),'\n')
+    cat('Test duration was',as.character(round(time.taken,2)),attr(time.taken, 'units'),'\n')
+    system(paste0('rm ',opt$output_dir,'*Res')) 
+    sink()
+    q()
 }
+  
+system(paste0('rm ',opt$output_dir,'GWAS_sumstats_COJO.txt'))
 
 ####
 # Calculate mean and sd of polygenic scores at each threshold
@@ -325,23 +393,38 @@ sink(file = paste(opt$output,'.log',sep=''), append = T)
 cat('Calculating polygenic scores in reference...')
 sink()
 
-system(paste0(opt$plink, ' --bfile ',opt$ref_plink,' --score ',opt$output_dir,'GWAS_sumstats_SBayesR.GW.snpRes 2 4 6 sum --out ',opt$output_dir,'ref.profiles --memory ',floor(opt$memory*0.7)))
+foreach(setting=settings) %dopar% {
+    
+    if (setting == ''){
+        prefix <- 'GWAS_sumstats_SBayesR'
+        profile_prefix <- paste0(opt$output_dir,'ref.profiles')
+        profile <- paste0(opt$output_dir,'ref.profiles.profile')
+    } else {
+        prefix <- paste0('GWAS_sumstats_SBayesR.', setting)
+        profile_prefix <- paste0(opt$output_dir,'ref.profiles.', setting)
+        profile <- paste0(opt$output_dir,'ref.profiles.',setting,'.profile')
+    }
+    
+    system(paste0(opt$plink, ' --bfile ',opt$ref_plink,' --score ',opt$output_dir,prefix,'.GW.snpRes 2 4 6 sum --out ',profile_prefix,' --memory ',floor(opt$memory*0.7)))
 
-# Read in the reference scores
-scores<-fread(paste0(opt$output_dir,'ref.profiles.profile'))
-
-# Calculate the mean and sd of scores for each population specified in pop_scale
-pop_keep_files<-read.table(opt$ref_pop_scale, header=F, stringsAsFactors=F)
-
-for(k in 1:dim(pop_keep_files)[1]){
-	pop<-pop_keep_files$V1[k]
-	keep<-fread(pop_keep_files$V2[k], header=F)
-	scores_keep<-scores[(scores$FID %in% keep$V1),]
-
-	ref_scale<-data.frame(	Mean=round(mean(scores_keep$SCORESUM),3),
-							SD=round(sd(scores_keep$SCORESUM),3))
-
-	fwrite(ref_scale, paste0(opt$output,'.',pop,'.scale'), sep=' ')
+    # Read in the reference scores
+    scores<-fread(profile)
+    
+    # Calculate the mean and sd of scores for each population specified in pop_scale
+    pop_keep_files<-read.table(opt$ref_pop_scale, header=F, stringsAsFactors=F)
+    
+    for(k in 1:dim(pop_keep_files)[1]){
+      pop<-pop_keep_files$V1[k]
+      keep<-fread(pop_keep_files$V2[k], header=F)
+      scores_keep<-scores[(scores$FID %in% keep$V1),]
+      ref_scale<-data.frame('Mean'=round(mean(scores_keep$SCORESUM),3), 'SD'=round(sd(scores_keep$SCORESUM),3))
+      if (setting == ''){
+          scale_out <- paste0(opt$output,'.',pop,'.scale')
+      } else {
+          scale_out <- paste0(opt$output,'.',setting,'.',pop,'.scale')
+      }
+      fwrite(ref_scale, scale_out, sep=' ')
+    }    
 }
 
 sink(file = paste(opt$output,'.log',sep=''), append = T)
