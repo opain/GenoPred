0c1fcc1a3d61dcc63c0a696edbf6b65e8f417011
Oliver Pain <oliver.pain@kcl.ac.uk>
2021-08-16 13:45:44 +0100

    Created GenoPredPipe, and made minor edits to scripts
    Former-commit-id: 3b93e298a2f4fc681ffa11606445a0e1fc5b3ac7

---
+++ GenoPredPipe/scripts/ancestry_reporter.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# Read in samples file
+config<-read.table('config_test.yaml', header=F)
+target_list<-read.table(as.character(config$V2[config$V1 == 'target_list:']), header=T)
+
+# Identify which super populations each target contain
+ancestry_report<-NULL
+target<-args[1]
+
+path<-paste0('resources/data/target/',target,'/ancestry')
+files<-list.files(path=path)
+keep_files<-files[grepl('.keep$',files) & grepl('model_pred',files)]
+
+info = file.info(paste0(path, '/', keep_files))
+not_empty = rownames(info[info$size != 0, ])
+  
+ancestry_report<-rbind(ancestry_report, data.frame(name=target,
+                                                   population=gsub('.keep','',gsub('.*model_pred.','',not_empty))))
+
+write.table(ancestry_report,paste0('resources/data/target/',target,'/ancestry_report.txt'), col.names=T, row.names=F, quote=F)
+
