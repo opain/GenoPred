be5b2759dd3fc3b000e8cfef5ec217cfef8bc435
Oliver Pain <oliver.pain@kcl.ac.uk>
2021-10-18 11:21:53 +0100

    Changed function for defining output directory internally to avoid error when output file name is present in the output path
    Former-commit-id: 68e3d15d29e34bba298502230672ecd237dedc02

--- Scripts/Population_outlier/Population_outlier.R
+++ Scripts/Population_outlier/Population_outlier.R
@@ -39,8 +39,7 @@ library(NbClust)
 library(factoextra)
 library(GGally)
 
-tmp<-sub('.*/','',opt$output)
-opt$output_dir<-sub(paste0(tmp,'*.'),'',opt$output)
+opt$output_dir<-dirname(opt$output)
 system(paste0('mkdir -p ',opt$output_dir))
 
 sink(file = paste(opt$output,'.log',sep=''), append = F)
