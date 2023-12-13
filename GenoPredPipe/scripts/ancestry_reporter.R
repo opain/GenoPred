6c407f6ac4a42c83250a27c74cbc5e8a63390bab
Ollie <olliepain@yahoo.com>
2023-12-13 15:39:49 +0000

    Refactored ancestry_identification code
    Former-commit-id: 934b569ba8a1aecd23c8c88f0d3d4b181b9dcd2a

--- GenoPredPipe/scripts/ancestry_reporter.R
+++ GenoPredPipe/scripts/ancestry_reporter.R
@@ -6,15 +6,21 @@ ancestry_report<-NULL
 target<-args[1]
 output<-args[2]
 
-path<-paste0(output,'/',target,'/ancestry')
-files<-list.files(path=path)
-keep_files<-files[grepl('.keep$',files) & grepl('model_pred',files)]
+path<-paste0(output,'/',target,'/ancestry/keep_files/model_based')
+keep_files<-list.files(path=path)
 
+# Create file list populations present in target
 info = file.info(paste0(path, '/', keep_files))
 not_empty = rownames(info[info$size != 0, ])
   
 ancestry_report<-rbind(ancestry_report, data.frame(name=target,
-                                                   population=gsub('.keep','',gsub('.*model_pred.','',not_empty))))
+                                                   population=gsub('.*\\/','',gsub('.keep','',not_empty))))
 
 write.table(ancestry_report,paste0(output,'/',target,'/ancestry/ancestry_report.txt'), col.names=T, row.names=F, quote=F)
 
+# Create a keep_list
+keep_list<-data.frame(
+    POP = gsub('.*\\/','',gsub('.keep','',not_empty)),
+    file = not_empty)
+
+write.table(keep_list, paste0(output,'/',target,'/ancestry/keep_list.txt'), col.names=T, row.names=F, quote=F)
--- Scripts/functions/misc.R
+++ Scripts/functions/misc.R
@@ -25,7 +25,6 @@ calc_score<-function(bfile, score, keep=NULL, extract=NULL, chr=1:22, frq=NULL, 
     if(n_scores > 1){
         cmd<-paste0(cmd, ' --score-col-nums 3-',2+n_scores)
     }
-    
     # Calculate score in the target sample
     for(i in chr){
         cmd_i<-gsub('CHROMOSOME_NUMBER',i,cmd)
@@ -148,4 +147,150 @@ read_bim<-function(dat, chr = 1:22){
   names(bim)<-c('CHR','SNP','BP','A1','A2')
 
   return(bim)
+}
+
+# Detect wether object refers to a data.frame or a file
+dat_or_file<-function(x, header = F){
+  if(is.null(x)){
+    file_path <- NULL
+  } else {
+    if (is.data.frame(x)) {
+      # Create file in temp directory
+      file_path<-tempfile()
+      write.table(x, file_path, col.names = header, row.names=F, quote=F)
+    } else if (is.character(x) && file.exists(x)) {
+      file_path <- x
+    } else {
+      stop("Input must be a data frame or a valid file path.")
+    }
+  }
+  return(file_path)
+}
+
+# Make a subset of plink1 binaries
+plink_subset<-function(plink='plink', chr = 1:22, keep = NA, bfile, out, memory = 4000, threads = 1){
+
+  # If object, create file
+  keep <- dat_or_file(keep)
+
+  # Prepare plink options
+  plink_opt<-NULL
+  if(!is.null(keep)){
+    plink_opt<-c(plink_opt, paste0('--keep ',keep,' '))
+  }
+
+  # Run plink
+  cmd <- paste0(opt$plink,' --bfile ',bfile,' --threads ', threads,' ',plink_opt,'--make-bed --out ',out,' --memory ',memory)
+  exit_status <- system(cmd, intern=FALSE)
+  if (exit_status != 0) {
+    cat("Error occurred in running plink command for chromosome\n")
+  }
+}
+
+# Return a list of variants surviving QC using plink
+plink_qc_snplist<-function(bfile, chr = 1:22, threads = 1, memory = 4000, geno = NULL, maf = NULL, hwe = NULL){
+  plink_opt<-NULL
+  if(!is.null(geno)){
+    plink_opt <- c(plink_opt, paste0('--geno ',geno,' '))
+  }
+  if(!is.null(maf)){
+    plink_opt <- c(plink_opt, paste0('--maf ',maf,' '))
+  }
+  if(!is.null(hwe)){
+    plink_opt <- c(plink_opt, paste0('--hwe ',hwe,' '))
+  }
+
+  temp_file<-tempfile()
+  snplist<-NULL
+  for(chr_i in chr){
+    system(paste0(opt$plink,' --bfile ',bfile,chr_i,' --threads ',threads,' ',plink_opt,' --write-snplist --out ', temp_file,' --memory ', memory))
+    snplist<-c(snplist, fread(paste0(temp_file, '.snplist'), header=F)$V1)
+  }
+
+  return(snplist)
+}
+
+# Coordinates of high ld regions
+long_ld_coord<-do.call(rbind, list(
+    data.frame(CHR = 1, P0 = 48e6, P1 = 52e6),
+    data.frame(CHR = 2, P0 = 86e6, P1 = 100.5e6),
+    data.frame(CHR = 2, P0 = 134.5e6, P1 = 138e6),
+    data.frame(CHR = 2, P0 = 183e6, P1 = 190e6),
+    data.frame(CHR = 3, P0 = 47.5e6, P1 = 50e6),
+    data.frame(CHR = 3, P0 = 83.5e6, P1 = 87e6),
+    data.frame(CHR = 3, P0 = 89e6, P1 = 97.5e6),
+    data.frame(CHR = 5, P0 = 44.5e6, P1 = 50.5e6),
+    data.frame(CHR = 5, P0 = 98e6, P1 = 100.5e6),
+    data.frame(CHR = 5, P0 = 129e6, P1 = 132e6),
+    data.frame(CHR = 5, P0 = 135.5e6, P1 = 138.5e6),
+    data.frame(CHR = 6, P0 = 25.5e6, P1 = 33.5e6),
+    data.frame(CHR = 6, P0 = 57e6, P1 = 64e6),
+    data.frame(CHR = 6, P0 = 140e6, P1 = 142.5e6),
+    data.frame(CHR = 7, P0 = 55e6, P1 = 66e6),
+    data.frame(CHR = 8, P0 = 8e6, P1 = 12e6),
+    data.frame(CHR = 8, P0 = 43e6, P1 = 50e6),
+    data.frame(CHR = 8, P0 = 112e6, P1 = 115e6),
+    data.frame(CHR = 10, P0 = 37e6, P1 = 43e6),
+    data.frame(CHR = 11, P0 = 46e6, P1 = 57e6),
+    data.frame(CHR = 11, P0 = 87.5e6, P1 = 90.5e6),
+    data.frame(CHR = 12, P0 = 33e6, P1 = 40e6),
+    data.frame(CHR = 12, P0 = 109.5e6, P1 = 112e6),
+    data.frame(CHR = 20, P0 = 32e6, P1 = 34.5e6)
+))
+
+# Remove variants within genomic regions (REF: PMC2443852)
+remove_regions<-function(bim, regions){
+  exclude<-NULL
+  for(i in 1:nrow(regions)){
+    exclude<-c(exclude, bim$SNP[(   bim$CHR == regions$CHR[i]  &
+                                    bim$BP >= regions$P0[i] &
+                                    bim$BP <= regions$P1[i])])
+  }
+
+  return(bim[!(bim$SNP %in% exclude),])
+}
+
+# Read in per chromosome bim files
+read_bim<-function(x){
+  bim<-NULL
+  for(chr_i in 1:22){
+      bim <- rbind(bim, fread(paste0(x, chr_i,'.bim')))
+  }
+  names(bim)<-c('CHR','SNP','POS','BP','A1','A2')
+  return(bim)
+}
+
+# Perform PCA using plink files
+plink_pca<-function(bfile, extract = NULL, flip = NULL, memory = 4000, n_pc = 6){
+  ###########
+  # Merge subset reference
+  ###########
+  tmp_dir<-tempdir()
+
+  # Create merge list
+  ref_merge_list<-paste0(bfile,1:22)
+  write.table(ref_merge_list, paste0(tmp_dir,'/ref_mergelist.txt'), row.names=F, col.names=F, quote=F)
+
+  # Merge
+  plink_opt<-NULL
+  if(!is.null(extract)){
+    write.table(extract, paste0(tmp_dir,'/extract.snplist'), col.names = F, row.names = F, quote=F)
+    plink_opt<-c(plink_opt, paste0('--extract ',tmp_dir,'/extract.snplist '))
+  }
+  if(!is.null(flip)){
+    write.table(flip, paste0(tmp_dir,'/flip_list.txt'), col.names = F, row.names = F, quote=F)
+    plink_opt<-c(plink_opt, paste0('--flip ',tmp_dir,'/flip_list.txt '))
+  }
+  cmd<-paste0(opt$plink,' --merge-list ',tmp_dir,'/ref_mergelist.txt ',plink_opt,'--threads 1 --make-bed --out ',tmp_dir,'/ref_merge --memory ',memory)
+  system(cmd)
+
+  # Calculate SNP weights
+  system(paste0(opt$plink2,' --bfile ',tmp_dir,'/ref_merge --threads 1 --pca ',n_pc,' biallelic-var-wts  --out ',tmp_dir,'/ref_merge --memory ', memory))
+
+  # Format the SNP-weights
+  snp_weights<-fread(paste0(tmp_dir,'/ref_merge.eigenvec.var'))
+  snp_weights<-snp_weights[,c(-1,-4),with=F]
+  names(snp_weights)[1:2]<-c('SNP','A1')
+
+  return(snp_weights)
 }
