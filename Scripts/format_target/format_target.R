9916175d0266bd2e1b7b3483e235b1a61a08a676
Ollie <olliepain@yahoo.com>
2024-03-01 16:01:43 +0000

    Included --allow-no-sex param in plink1 when merging to avoid error when target includes sex ambiguous individuals

--- Scripts/format_target/format_target.R
+++ Scripts/format_target/format_target.R
@@ -190,7 +190,7 @@ system(paste0(opt$plink2,' --pfile ',opt$ref,' --make-pgen --update-ids ',tmp_di
 # In the meantime, convert the ref and target into plink1 binaries, merge, and then convert back to plink2 binaries
 system(paste0(opt$plink2,' --pfile ',tmp_dir,'/subset --make-bed --memory 5000 --threads 1 --out ',tmp_dir,'/subset'))
 system(paste0(opt$plink2,' --pfile ',opt$ref,' --make-bed --out ',tmp_dir,'/REF --memory 5000 --threads 1'))
-system(paste0(opt$plink,' --bfile ',tmp_dir,'/subset --bmerge ',tmp_dir,'/REF --make-bed --out ',tmp_dir,'/ref_targ'))
+system(paste0(opt$plink,' --bfile ',tmp_dir,'/subset --bmerge ',tmp_dir,'/REF --make-bed --allow-no-sex --out ',tmp_dir,'/ref_targ'))
 
 # Extract only target individuals
 system(paste0(opt$plink2,' --bfile ',tmp_dir,'/ref_targ --remove ',tmp_dir,'/REF.psam --make-pgen --memory 5000 --threads 1 --out ',opt$output))
